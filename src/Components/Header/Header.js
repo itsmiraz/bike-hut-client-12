5f57f579efe6a992ef1675ce339234a6a6c099a9
itsmiraz <mirajulislamofficial044@gmail.com>
2022-11-23 22:08:57 +0600

    headers done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import logo from '../../assets/Logo/BIKEHUTLOGO.png'
+
 
 const Header = () => {
+    const [open, setOpen] = useState(false)
     return (
-        <div>
-            header
-        </div>
+        <div className='bg-white  text-slate-900 items-center md:px-10 px-4 py-3 flex justify-between w-full'>
+
+           <img className='w-40' src={logo} alt="" />
+
+            <div>
+                <ul className={`md:flex bg-white right-0 z-0 items-center w-full text-slate-900 text-center justify-center md:static duration-300 ease-linear absolute ${open ? 'top-12' : 'top-[-450px]'}`}>
+                    <li className='font-semibold mr-4'>
+                        <Link>Home</Link>
+                    </li>
+                    <li className='font-semibold mr-4'>
+                        <Link>Rooms</Link>
+                    </li>
+                    <li className='font-semibold mr-4'>
+                        <Link>Books</Link>
+                    </li>
+                    <li className='font-semibold mr-4'>
+                    <button type="button" className="px-4 py-2 font-semibold rounded-full border border-teal-500 text-gray-800">Sign In</button>
+                    </li>
+                    <li className='font-semibold mr-4'>
+                    <button type="button" className="px-4 py-2 font-semibold rounded-full bg-teal-500 text-white">Sing Out</button>
+                    </li>
+
+                </ul>
+                <div onClick={() => setOpen(!open)} className="h-8 p-1 rounded-full bg-teal-500 text-white w-8 md:hidden" >
+                    {open ? <XMarkIcon />
+                        : <Bars3Icon />
+                    }
+                </div>
+            </div>
+        </div >
     );
 };
 
